6be1b0b75daeadd22830a6c51a359ec36bfccd28
GerstnerJuanI <43797469+GerstnerJuanI@users.noreply.github.com>
2021-07-19 23:43:13 -0300

    clase 16

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -24,6 +24,13 @@ class CursoController extends Controller
     }
     
     public function store(Request $request){
+
+        $request->validate([
+            'name'=> 'required|max:10',
+            'descripcion' => 'required|min:10',
+            'categoria' => 'required'
+            ]);
+
         $curso = new Curso();
 
         $curso->name = $request->name;
@@ -42,6 +49,12 @@ class CursoController extends Controller
 
     public function update(Request $request, Curso $curso)
     {
+        $request->validate([
+            'name'=> 'required',
+            'descripcion' => 'required',
+            'categoria' => 'required'
+            ]);
+
         $curso->name = $request->name;
         $curso->descripcion = $request->descripcion;
         $curso->categoria = $request->categoria;
--- resources/views/cursos/edit.blade.php
+++ resources/views/cursos/edit.blade.php
@@ -7,27 +7,47 @@
 <form action="{{route('cursos.update', $curso)}}" method="POST">
 
     @csrf {{--token  --}}
-    @method('put')
+
+    @method('put') {{--define que es para actualizar datos.--}}
+
     <label>
         Nombre:
         <br>
-        <input type="text" name="name" value="{{$curso->name}}">
+        <input type="text" name="name" value="{{old('name',$curso->name)}}">
     </label>
+
+    @error('name')
+        <br>
+        <small>*{{$message}}</small>
+        <br>
+    @enderror
 
     <br>
     <label>
         Descripcion:
         <br>
-        <textarea name="descripcion" rows="5">{{$curso->descripcion}}</textarea>
+        <textarea name="descripcion" rows="5" >{{old('descripcion',$curso->decripcion)}}</textarea>
     </label>
+
+    @error('descripcion')
+        <br>
+        <small>{{$message}}</small>
+        <br>
+    @enderror
 
     <br>
     <label>
         Categoria:
         <br>
-        <input type="text" name="categoria" value="{{$curso->categoria}}">
+        <input type="text" name="categoria" value="{{old('categoria',$curso->categoria)}}">
     </label>
     
+    @error('categoria')
+        <br>
+        <small>{{$message}}</small>
+        <br>
+    @enderror
+
     <br>
     <button type="submit">Actualizar Formulario</button>
 </form>
